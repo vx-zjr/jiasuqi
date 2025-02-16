472b38ce2ea4d39ba7503431de32eaf8f8dda3bd
vx <vx.zjr.v@gmail.com>
2025-02-16 16:42:34 +0800

    5

---
+++ ios/MyApp/ViewController.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "Hello World!"
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .white
+        title = "首页"
+        
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+} 
